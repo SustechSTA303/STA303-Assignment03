18b89f977544344473a8b700375f3ef2bc019cf1
Kotobuki Mugi <1804502127@qq.com>
2023-12-12 14:38:02 +0800

    1

--- find_shortest_path.py
+++ find_shortest_path.py
@@ -4,26 +4,75 @@ from build_data import Station, build_data
 import argparse
 
 
-# Implement the following function
+### 
+from typing import List
+import heapq
+
+class Node:
+    def __init__(self, station, g, h, parent=None):
+        self.station = station
+        self.g = g  # Actual cost from the start node
+        self.h = h  # Heuristic cost to the goal node
+        self.parent = parent  # Parent node for constructing the path
+
+    def f(self):
+        return self.g + self.h  # Total cost
+
+def heuristic(station1, station2):
+    # Use Euclidean distance as the heuristic function
+    return ((station1.position[0] - station2.position[0]) ** 2 +
+            (station1.position[1] - station2.position[1]) ** 2) ** 0.5
+
+def astar(start_station, end_station, map):
+    open_set = []
+    closed_set = set()
+
+    start_node = Node(start_station, 0, heuristic(start_station, end_station))
+    heapq.heappush(open_set, (start_node.f(), start_node))
+
+    while open_set:
+        _, current_node = heapq.heappop(open_set)
+
+        if current_node.station == end_station:
+            path = []
+            while current_node:
+                path.insert(0, current_node.station.name)
+                current_node = current_node.parent
+            return path
+
+        closed_set.add(current_node.station)
+
+        for neighbor_station in current_node.station.links:
+            if neighbor_station in closed_set:
+                continue
+
+            tentative_g = current_node.g + 1  # Assuming uniform cost for simplicity
+
+            neighbor_node = Node(
+                station=neighbor_station,
+                g=tentative_g,
+                h=heuristic(neighbor_station, end_station),
+                parent=current_node
+            )
+
+            if any(neighbor_node.station == node.station for _, node in open_set):
+                continue
+
+            heapq.heappush(open_set, (neighbor_node.f(), neighbor_node))
+
+    return None  # No path found
+
+
 def get_path(start_station_name: str, end_station_name: str, map: dict[str, Station]) -> List[str]:
-    """
-    runs astar on the map, find the shortest path between a and b
-    Args:
-        start_station_name(str): The name of the starting station
-        end_station_name(str): str The name of the ending station
-        map(dict[str, Station]): Mapping between station names and station objects of the name,
-                                 Please refer to the relevant comments in the build_data.py
-                                 for the description of the Station class
-    Returns:
-        List[Station]: A path composed of a series of station_name
-    """
-    # You can obtain the Station objects of the starting and ending station through the following code
     start_station = map[start_station_name]
     end_station = map[end_station_name]
-    # Given a Station object, you can obtain the name and latitude and longitude of that Station by the following code
-    print(f'The longitude and latitude of the {start_station.name} is {start_station.position}')
-    print(f'The longitude and latitude of the {end_station.name} is {end_station.position}')
-    pass
+
+    path = astar(start_station, end_station, map)
+
+    if path:
+        return path
+    else:
+        return ["No path found"]
 
 
 if __name__ == '__main__':
