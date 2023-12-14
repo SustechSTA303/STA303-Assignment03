4047b412fd914e597452e39b79436b274d5f5b15
liyibo <gqlx567@qq.com>
2023-12-14 10:35:48 +0000

    first commit

--- find_shortest_path.py
+++ find_shortest_path.py
@@ -2,28 +2,19 @@ from typing import List
 from plot_underground_path import plot_path
 from build_data import Station, build_data
 import argparse
+import heapq 
+import algorithm  #the algorithms are in this file.
+import queue
+import time
 
 
-# Implement the following function
-def get_path(start_station_name: str, end_station_name: str, map: dict[str, Station]) -> List[str]:
-    """
-    runs astar on the map, find the shortest path between a and b
-    Args:
-        start_station_name(str): The name of the starting station
-        end_station_name(str): str The name of the ending station
-        map(dict[str, Station]): Mapping between station names and station objects of the name,
-                                 Please refer to the relevant comments in the build_data.py
-                                 for the description of the Station class
-    Returns:
-        List[Station]: A path composed of a series of station_name
-    """
-    # You can obtain the Station objects of the starting and ending station through the following code
-    start_station = map[start_station_name]
-    end_station = map[end_station_name]
-    # Given a Station object, you can obtain the name and latitude and longitude of that Station by the following code
-    print(f'The longitude and latitude of the {start_station.name} is {start_station.position}')
-    print(f'The longitude and latitude of the {end_station.name} is {end_station.position}')
-    pass
+
+Cost=['UNIT','DISTANCE'] #UNIT represents that all cost between two adjacent stations is 1. DISTANCE represents using the realistic distance as the cost.
+algorithms=dict()
+algorithms[algorithm.get_path_A_Manhatton]="A*_Manhatton"
+algorithms[algorithm.get_path_A_L2]="A*_Euclidean"
+algorithms[algorithm.get_path_dijkstra]="Dijkstra"
+algorithms[algorithm.get_path_bfs]="BFS"   
 
 
 if __name__ == '__main__':
@@ -39,7 +30,19 @@ if __name__ == '__main__':
 
     # The relevant descriptions of stations and underground_lines can be found in the build_data.py
     stations, underground_lines = build_data()
-    path = get_path(start_station_name, end_station_name, stations)
+    with open("output.txt","a") as f:
+        f.write(f"the start station is {start_station_name},the end station is {end_station_name}.\n")
+        
+    for cost in Cost:
+        for key in algorithms:
+            time_start=time.time()
+            path=key(start_station_name, end_station_name, stations,cost)
+            time_end=time.time()
+            print(f"the cost time is {(time_end-time_start)*1000} ms, the cost is calculated through {cost}, the algorithm used is {algorithms[key]}. The length of the path is {len(path)}. ")
+            with open("output.txt","a") as f:
+                f.write(f"the cost time is {(time_end-time_start)*1000} ms, the cost is calculated through {cost}, the algorithm used is {algorithms[key]}.The length of the path is {len(path)}.\n ")
+            
+        
     # visualization the path
     # Open the visualization_underground/my_path_in_London_railway.html to view the path, and your path is marked in red
     plot_path(path, 'visualization_underground/my_shortest_path_in_London_railway.html', stations, underground_lines)
