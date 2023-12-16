10c43518128d765d45315f592cf6962b32d595c5
Nolan77777 <118642865+Nolan77777@users.noreply.github.com>
2023-12-16 18:41:17 +0800

    Update build_data.py

--- build_data.py
+++ build_data.py
@@ -13,10 +13,6 @@ class Station:
         self.name = name
         self.position = position
         self.links = set()
-    
-    def __lt__(self, other):
-        return self.id < other.id
-
 
 def build_data():
     """
